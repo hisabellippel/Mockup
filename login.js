847044296bacdbeb311d800bcd047cf1a961683a
Julia Izabel <Julia_roza@estudante.sesisenai.org.br>
2025-04-01 12:52:49 -0300

    add const

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
-
-
+function validarLogin() {
+    const name = document.getElementById ('nome').ariaValueMax.trim();
+    const credencial = document.getElementById('credencial').ariaValueMax.trim();
 
 oi
 const OTP = parseInt(document.getElementById('OTP').value, 10);
