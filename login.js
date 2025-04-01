ae32616e4c3083604503c77f2aa517a38c9dde7d
Julia Izabel <Julia_roza@estudante.sesisenai.org.br>
2025-04-01 12:55:50 -0300

    add nome

--- login.js
+++ login.js
@@ -4,3 +4,10 @@ function validarLogin() {
 
 const senha = document.getElementById('senha_usuario').value.trim();
 const OTP = parseInt(document.getElementById('OTP').value, 10);
+ if (!nome){
+    alert ('Por favor, preencher o nome');
+    return;
+ }else{
+    console.log (nome);
+ }
+ }
